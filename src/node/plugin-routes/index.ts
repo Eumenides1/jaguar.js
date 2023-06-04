a484d8adc94a9cf8146debef60edb4893ac9532e
jaguarliu <jaguarliu@tencent.com>
2023-06-04 18:22:40 +0800

    feat: 共享编译时信息

--- src/node/plugin-routes/index.ts
+++ src/node/plugin-routes/index.ts
@@ -1,5 +1,6 @@
 import { Plugin } from 'vite';
 import { RouteService } from './RouteService';
+import { PageModule } from 'shared/types';
 import React from 'react';
 // 本质: 把文件目录结构 -> 路由数据
 
@@ -7,6 +8,7 @@ export interface Route {
   path: string;
   element: React.ReactElement;
   filePath: string;
+  preload: () => Promise<PageModule>;
 }
 
 interface PluginOptions {
